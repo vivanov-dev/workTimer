4ea5cb23a7359c1bc66a9d6b36e547a292b9a96f
Vladyslav Ivanov <vlad.i999913@gmail.com>
2024-08-25 17:48:17 +0300

    feat: test version

--- main.js
+++ main.js
@@ -1,17 +1,34 @@
-import './style.css'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+const launchDate = new Date('Sept 7 2024').getTime();
+const daysElement = document.getElementById('days');
+const hoursElement = document.getElementById('hours');
+const minutesElement = document.getElementById('minutes');
+const secondsElement = document.getElementById('seconds');
+const captionElement = document.getElementById('caption');
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+const countdown = function () {
+  const presentDate = new Date().getTime();
+  const timeDifference = launchDate - presentDate;
 
-setupCounter(document.getElementById('counter'));
+  if (timeDifference > 0) {
+    const diffInSeconds = Math.floor(timeDifference / 1000);
+    const days = Math.floor(diffInSeconds / 86400);
+    const hours = Math.floor((diffInSeconds % 86400) / 3600);
+    const minutes = Math.floor((diffInSeconds % 3600) / 60);
+    const seconds = diffInSeconds % 60;
+
+    if (daysElement) daysElement.innerText = zeroPadding(days);
+    if (hoursElement) hoursElement.innerText = zeroPadding(hours);
+    if (minutesElement) minutesElement.innerText = zeroPadding(minutes);
+    if (secondsElement) secondsElement.innerText = zeroPadding(seconds);
+  } else {
+    if (captionElement) captionElement.innerText = '🎉Congratulations!🎉';
+  }
+};
+
+function zeroPadding(time) {
+  return time < 10 ? '0' + time : time;
+}
+
+setInterval(countdown, 1000);
+
+countdown();
